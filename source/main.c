ac7c1542776ad68ae4fbae30bdce82cdeafd5eea
Such Meme, Many Skill <owen.vanrijn@gmail.com>
2019-11-02 22:52:48 +0100

    Add (crude) argument payload launching

--- source/main.c
+++ source/main.c
@@ -325,6 +325,10 @@ void main_menu(){
 int main(int argc, char* argv[])
 {
     consoleInit(NULL);
+    
+    if (argc > 1)
+        if (access(argv[1], F_OK) != -1)
+            reboot(argv[1]);
 
     if (access("payload_launcher.ini", F_OK) != -1) loadini();
 
